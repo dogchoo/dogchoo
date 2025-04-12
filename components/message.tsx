3d885e4d8f15769cc615d6417d0f1760c6d31bc8
이준하 <82079706+codma1123@users.noreply.github.com>
2025-04-12 18:09:01 +0900

    메시지 위치 변경

--- components/message.tsx
+++ components/message.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MessageListItem } from "@/features/message/model/types/message";
+import { MessageListItem } from "@/features/message/model/types/message-list-item";
 import { useClient } from "@/hooks/use-client";
 import { cn } from "@/libs/utils";
 import { motion } from "framer-motion";
@@ -12,14 +12,14 @@ interface MessageProps {
 
 const Message = ({ className, message }: MessageProps) => {
   const clientId = useClient();
+  const isCurrentClient = message.clientId === clientId;
 
-  console.log(clientId, message.clientId);
   return (
     <motion.div
-      className={cn("w-fit rounded-md border p-2", className, message.clientId === clientId && "self-end")}
-      initial={{ opacity: 0, translateX: -20 }}
-      animate={{ opacity: 1, translateX: -2 }}
-      exit={{ opacity: 0, translateX: -20 }}
+      className={cn("w-fit rounded-md border p-2", className, isCurrentClient && "self-end")}
+      initial={{ opacity: 0, translateX: isCurrentClient ? -20 : 20 }}
+      animate={{ opacity: 1, translateX: isCurrentClient ? -2 : 2 }}
+      exit={{ opacity: 0, translateX: isCurrentClient ? -20 : 20 }}
     >
       <div className="flex items-center justify-between gap-2">
         <p className="text-sm">{message.content}</p>
