111204fa4ce98609efff475b44ab09e83e1a29a3
이준하 <82079706+codma1123@users.noreply.github.com>
2025-04-13 19:09:51 +0900

    배포오류 ㅜㅜ

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    loader: "imgix",
+    path: "https://dogchoo-rainchoo.web.app/",
+  },
 };
 
 export default nextConfig;
