5f49137714d26854ef83b746e93226992ce3387c
이준하 <82079706+codma1123@users.noreply.github.com>
2025-04-13 19:40:28 +0900

    호스팅 설정

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   databaseURL: process.env.NEXT_PUBLIC_URL_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_URL_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_URL_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_URL_MESSING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_URL_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_URL_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_URL_MEASUREMENT_ID,
 };
