a0251718d34dfa07968af600e40fe188810311f4
이준하 <82079706+codma1123@users.noreply.github.com>
2025-04-18 18:52:13 +0900

    자연스러운 엔터 처리 구현

--- app/topic/[id]/chat/(components)/chat-input-form.tsx
+++ app/topic/[id]/chat/(components)/chat-input-form.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { CreateMessageFormValue, createMessageSchema } from "@/features/message/model/schema/create-message-schema";
+import { Textarea } from "@/components/ui/textarea";
+import { CreateMessageFormValue, createMessageSchema, MAX_MESSAGE_CONTENT_LENGTH } from "@/features/message/model/schema/create-message-schema";
 import { useClient } from "@/hooks/use-client";
+import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AnimatePresence, motion } from "framer-motion";
-import { LoaderCircleIcon, SendIcon } from "lucide-react";
+import { LoaderCircleIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -52,23 +52,38 @@ const ChatInputForm = ({ isChatEnabled, isLoading, handleSubmit }: ChatInputForm
               <FormItem className="flex-1">
                 <FormControl>
                   <div className="relative">
-                    <Input
+                    <Textarea
                       autoFocus
-                      className="h-14 text-sm"
+                      className="border-input bg-background placeholder:text-muted-foreground focus-visible:ring-ring h-14 max-h-60 min-h-14 w-full resize-none overflow-y-hidden rounded-md border px-3 py-2 pr-16 text-sm shadow-sm focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                       {...field}
                       placeholder="채팅 메시지를 입력 해 주세요."
                       value={field.value || ""}
                       disabled={!isChatEnabled}
+                      onInput={(e) => {
+                        const target = e.target as HTMLTextAreaElement;
+                        target.style.height = "auto";
+                        target.style.height = `${target.scrollHeight}px`;
+
+                        if (target.value.length <= MAX_MESSAGE_CONTENT_LENGTH) {
+                          field.onChange(e);
+                        } else {
+                          target.value = target.value.slice(0, MAX_MESSAGE_CONTENT_LENGTH);
+                        }
+                      }}
+                      onKeyDown={(e) => {
+                        if (e.nativeEvent.isComposing) return;
+
+                        if (e.key === "Enter" && !e.shiftKey) {
+                          e.preventDefault();
+                          form.handleSubmit(onSubmit)();
+                        }
+                      }}
                     />
-                    <Button
-                      className="text-muted-foreground absolute top-1/2 right-4 flex -translate-y-1/2 items-center gap-x-2"
-                      size="sm"
-                      type="submit"
-                      variant="outline"
-                    >
-                      <SendIcon className="size-3" />
-                      <p>전송</p>
-                    </Button>
+
+                    <p className={cn("text-muted-foreground absolute top-2 right-2 text-xs", (field.value?.length || 0) === MAX_MESSAGE_CONTENT_LENGTH && "text-rose-500")}>
+                      ({field.value?.length || 0} /{MAX_MESSAGE_CONTENT_LENGTH})
+                    </p>
+
                     {isLoading && <LoaderCircleIcon className="text-muted-foreground absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin opacity-70" />}
                   </div>
                 </FormControl>
